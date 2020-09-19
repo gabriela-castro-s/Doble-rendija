fc867cbd6dcf9f13782d72f81b47a3eceb9409a6
Gabriela Castro Santamaría <69209287+gabriela-castro-s@users.noreply.github.com>
2020-09-19 12:46:30 -0500

    Add files via upload

---
+++ classicalToQuantum.py
@@ -0,0 +1,62 @@
+import matplotlib.pyplot as plot
+import numpy as np
+
+from calculadora_imaginarios import *
+
+
+def finalMatrix(matrix):
+    row, column = len(matrix), len(matrix[0])
+    for i in range(row):
+        nRow = []
+        for j in range(column):
+            nRow.append([(modulo(matrix[i][j]) ** 2), 0])
+
+        matrix[i] = nRow
+    return matrix
+
+
+def quantumProbabilisticSystem(matrix, vectIni, clicks):
+    if (clicks >= 0) and (type(clicks) is int):
+        length = len(vectIni)
+        copyMatrix = matrix[:]
+
+        for x in range(clicks):
+            matrix = multimatriz(matrix, copyMatrix)
+
+        return finalMatrix(matrix)
+    return -1
+
+
+def probabilisticSystem(matrix, vectIni, clicks):
+    if (clicks >= 0) and (type(clicks) is int):
+        length = len(vectIni)
+        for x in range(clicks):
+            vectIni = accionmatrizvector(matrix, vectIni)
+        return vectIni
+    return -1
+
+
+def experimentBooleanMatrix(clicks, booleanMatrix, vectIni):
+    if (clicks >= 0 and type(clicks) is int):
+        for c in range(clicks):
+            vectIni = accionvectormatrizboolean(booleanMatrix, vectIni)
+
+        return vectIni
+
+
+def multipleSlitExperiment(matrix, vectIni, clicks):
+    return probabilisticSystem(matrix, vectIni, clicks)
+
+
+def multipleSlitQuantumExperiment(matrix, vectIni, clicks):
+    return quantumProbabilisticSystem(matrix, vectIni, clicks)
+
+
+def graphProbabilitiesVector(vector):
+    data = len(vector)
+    x = np.array([x for x in range(data)])
+    y = np.array([round(vector[x][0] * 100, 2) for x in range(data)])
+
+    plot.bar(x, y, color='b', align='center')
+    plot.title('Probabilidades vector')
+    plot.show()
